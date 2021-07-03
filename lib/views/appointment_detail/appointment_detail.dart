cae6f1f8c0ad526cb111fd0695728615a317231a
TanPM77 <tanphamm2707@gmail.com>
2021-07-03 14:25:39 +0700

    Add popup in appointment detail when cancel

--- lib/views/appointment_detail/appointment_detail.dart
+++ lib/views/appointment_detail/appointment_detail.dart
@@ -25,8 +25,9 @@ class AppoinmentDetailScreen extends StatefulWidget {
       _AppoinmentDetailScreenState(appointment);
 }
 
-class _AppoinmentDetailScreenState extends State<AppoinmentDetailScreen> {
+class _AppoinmentDetailScreenState extends State<AppoinmentDetailScreen>{
   final Appointment appointment;
+  Reason _reason = Reason.onBussiness;
   _AppoinmentDetailScreenState(this.appointment);
   @override
   Widget build(BuildContext context) {
@@ -230,7 +231,27 @@ class _AppoinmentDetailScreenState extends State<AppoinmentDetailScreen> {
                   width: 70,
                 ),
                 FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              insetPadding: EdgeInsets.all(10),
+                              content: Stack(
+                                overflow: Overflow.visible,
+                                alignment: Alignment.center,
+                                children: [
+                                  Container(
+                                    width: double.infinity,
+                                    height: 450,
+                                    child: ReasonRadio(),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
+                      );
+                    },
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(20),
                         side: BorderSide(color: Colors.black)),
@@ -248,6 +269,155 @@ class _AppoinmentDetailScreenState extends State<AppoinmentDetailScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+enum Reason { onBussiness, wrongDay, orther, wrongService }
+
+class ReasonRadio extends StatefulWidget {
+  ReasonRadio({Key key}) : super(key: key);
+
+  @override
+  _ReasonRadioState createState() => _ReasonRadioState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _ReasonRadioState extends State<ReasonRadio> {
+  Reason _character = Reason.onBussiness;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text('Cho chúng tôi biết lý do', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+        SizedBox(height: 10,),
+        Text('Chọn các lý do sau'),
+        RadioListTile<Reason>(
+          title: Text('Bận công việc riêng'),
+          value: Reason.onBussiness,
+          groupValue: _character,
+          onChanged: (Reason value) {
+            setState(() {
+              _character = value;
+            });
+          },
+        ),
+        RadioListTile<Reason>(
+          title: const Text('Nhấn đặt nhầm ngày'),
+          value: Reason.wrongDay,
+          groupValue: _character,
+          onChanged: (Reason value) {
+            setState(() {
+              _character = value;
+            });
+          },
+        ),
+        RadioListTile<Reason>(
+          title: const Text('Nhấn đặt nhầm dịch vụ'),
+          value: Reason.wrongService,
+          groupValue: _character,
+          onChanged: (Reason value) {
+            setState(() {
+              _character = value;
+            });
+          },
+        ),
+        RadioListTile<Reason>(
+          title: const Text('Khác'),
+          value: Reason.orther,
+          groupValue: _character,
+          onChanged: (Reason value) {
+            setState(() {
+              _character = value;
+            });
+          },
+        ),
+      SizedBox(height: 10,),
+      TextFormField(
+        maxLines: 3,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5)
+            ),
+          hintText: 'Nếu chọn Khác, ghi lý do (optional)'
+        ),
+      ),
+      SizedBox(height: 10,),
+      Row(
+          children: [
+            SizedBox(
+              width: 25,
+            ),
+            RaisedButton(
+              onPressed: () =>
+                  Navigator.of(context).pop(),
+              shape: RoundedRectangleBorder(
+                  borderRadius:
+                  BorderRadius.circular(80.0)),
+              textColor: Colors.white,
+              padding: const EdgeInsets.all(0),
+              child: Container(
+                alignment: Alignment.center,
+                height: 50.0,
+                width: size.width * 0.25,
+                decoration: new BoxDecoration(
+                  borderRadius:
+                  BorderRadius.circular(80.0),
+                  gradient: new LinearGradient(colors: [
+                    Constants.PRIMARY_COLOR,
+                    Constants.HEAVY_BLUE
+                    // Color.fromARGB(255, 255, 136, 31),
+                    // Color.fromARGB(255, 255, 136, 31)
+                  ]),
+                ),
+                padding: const EdgeInsets.all(0),
+                child: Text(
+                  "Hủy bỏ",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 30,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                  borderRadius:
+                  BorderRadius.circular(80.0)),
+              textColor: Colors.white,
+              padding: const EdgeInsets.all(0),
+              child: Container(
+                alignment: Alignment.center,
+                height: 50.0,
+                width: size.width * 0.25,
+                decoration: new BoxDecoration(
+                  borderRadius:
+                  BorderRadius.circular(80.0),
+                  gradient: new LinearGradient(colors: [
+                    Constants.PRIMARY_COLOR,
+                    Constants.HEAVY_BLUE
+                    // Color.fromARGB(255, 255, 136, 34),
+                    // Color.fromARGB(255, 255, 177, 41)
+                  ]),
+                ),
+                padding: const EdgeInsets.all(0),
+                child: Text(
+                  "Gửi",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
